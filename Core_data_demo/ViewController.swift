3098d0f02e3d9d53f1a89798f7468efea7f97955
jisung <jisung.smart@gmail.com>
2019-09-30 13:05:22 +0900

    Initial Commit

---
+++ Core_data_demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core_data_demo
+//
+//  Created by The book Air on 30/09/2019.
+//  Copyright © 2019 jisung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
